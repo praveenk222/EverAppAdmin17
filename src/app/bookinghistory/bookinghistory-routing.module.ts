e58049b3325ac4b8e09d5535d47e500241a57cb8
praveenk222 <praveenkadma222@gmail.com>
2024-01-20 08:43:49 +0530

    dd

--- src/app/bookinghistory/bookinghistory-routing.module.ts
+++ src/app/bookinghistory/bookinghistory-routing.module.ts
@@ -4,12 +4,14 @@ import { BookinghistoryComponent } from './bookinghistory.component';
 import { HubBookingSummaryComponent } from './hub-booking-summary/hub-booking-summary.component';
 import { AllEbikeBookingComponent } from './all-ebike-booking/all-ebike-booking.component';
 import { InvoicepageComponent } from './invoicepage/invoicepage.component';
+import { BookingDetailsUserComponent } from './booking-details-user/booking-details-user.component';
 
 const routes: Routes = [
   { path: '', component: BookinghistoryComponent },
   { path: 'hub_booking', component: HubBookingSummaryComponent },
   { path: 'ebike_booking', component: AllEbikeBookingComponent },
   { path: 'invoice', component: InvoicepageComponent },
+  { path: 'user_booking_details', component: BookingDetailsUserComponent },
 ];
 
 @NgModule({
