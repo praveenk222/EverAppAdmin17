5f3bb4cbadbb8d4f1c03463f1a744a6942232ca0
praveenk222 <praveenkadma222@gmail.com>
2024-01-26 23:57:18 +0530

    s

--- src/app/manualbooking/manualbooking-routing.module.ts
+++ src/app/manualbooking/manualbooking-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ManualbookingComponent } from './manualbooking.component';
 import { ManualBookingBikelistComponent } from './manual-booking-bikelist/manual-booking-bikelist.component';
+import { ManualBookingProductdetailsComponent } from './manual-booking-productdetails/manual-booking-productdetails.component';
+import { ManualBookingUsersdetailsComponent } from './manual-booking-usersdetails/manual-booking-usersdetails.component';
 
 const routes: Routes = [
   { path: '', component: ManualbookingComponent },
-  { path: 'bike-list', component: ManualBookingBikelistComponent },
-  { path: '', component: ManualbookingComponent }
+  { path: 'bike_list', component: ManualBookingBikelistComponent },
+  { path: 'productdetails', component: ManualBookingProductdetailsComponent },
+  { path: 'userdetails', component: ManualBookingUsersdetailsComponent },
 ];
 
 @NgModule({
