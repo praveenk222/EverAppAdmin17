4b05610336f35951d7b76724f1886d7a41727d3b
nimmalapalli <nimmalapallinarendra@gmail.com>
2024-02-06 22:16:56 +0530

    code

--- src/app/complains/complains.component.ts
+++ src/app/complains/complains.component.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { UsersService } from '../services/users.service';
 import { AddTicketComponent } from './add-ticket/add-ticket.component';
 import { MatDialog } from '@angular/material/dialog';
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
 
 @Component({
   selector: 'app-complains',
@@ -10,13 +12,20 @@ import { MatDialog } from '@angular/material/dialog';
 })
 export class ComplainsComponent {
   complainsData:any;
+  displayedColumns: string[] = ['Booking id', 'User name', 'Mobile no.', 'Booking Date', 'Station Location', 'Wash time','Fare','Status','Reviews & ratings'];
+  dataSource = new MatTableDataSource<any>();
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  pageSizeOptions: number[] = [5, 10, 20];
+  pageSize = 5; //
 constructor(private us:UsersService,private dialog:MatDialog){
 this.complains()
 }
 
  complains(){
   this.us.getComplainsList().subscribe((res)=>{
-    this.complainsData=res
+   
+    this.complainsData = res;
+
    console.log(res)
   })
  }
@@ -32,5 +41,9 @@ this.complains()
     
   })
 
+}
+changePage(pageEvent: PageEvent) {
+  const startIndex = pageEvent.pageIndex * pageEvent.pageSize;
+  const endIndex = startIndex + pageEvent.pageSize;
 }
 }
