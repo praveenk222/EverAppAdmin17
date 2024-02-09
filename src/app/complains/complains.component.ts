73e3289a27b5aec471935e181705af952daf3bed
praveenk222 <praveenkadma222@gmail.com>
2024-02-10 03:24:14 +0530

    dd

--- src/app/complains/complains.component.ts
+++ src/app/complains/complains.component.ts
@@ -4,6 +4,7 @@ import { AddTicketComponent } from './add-ticket/add-ticket.component';
 import { MatDialog } from '@angular/material/dialog';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { SharedsameApiDataService } from '../services/sharedsame-api-data.service';
 
 @Component({
@@ -20,6 +21,11 @@ export class ComplainsComponent {
   pageSizeOptions: number[] = [5, 10, 20];
   pageSize = 5; //
   @ViewChild(MatPaginator) paginator: MatPaginator;
+
+  form = new FormGroup({
+    fromDate: new FormControl(null, { validators: [Validators.required]}),
+    toDate: new FormControl(null, { validators: [Validators.required]})
+  });
   response: any;
 constructor(private us:UsersService,private dialog:MatDialog,private shareds:SharedsameApiDataService){
 this.complains();
@@ -59,10 +65,19 @@ changePage(pageEvent: PageEvent) {
   const startIndex = pageEvent.pageIndex * pageEvent.pageSize;
   const endIndex = startIndex + pageEvent.pageSize;
 }
+applyDateFilter() {
+  if(this.dateRange.value.start && this.dateRange.value.end){
+    this.dataSource.data = this.arrayList;
+    console.log("data",this.dataSource.data);
+    this.dataSource.data = this.dataSource.data.filter(e=> new Date(e.timestamp).getTime() >= this.dateRange.value.start?.getTime()! && new Date(e.timestamp).getTime() <= this.dateRange.value.end?.getTime()!);
+    console.log("filterd",this.dataSource.data);
+  }
 
+}
 // search
-applyFilter(event: Event) {debugger
+applyFilter(event: Event) {
   const filterValue = (event.target as HTMLInputElement).value;
   this.dataSource.filter = filterValue.trim().toLowerCase();
 }
+
 }
