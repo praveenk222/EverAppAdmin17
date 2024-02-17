52d1107fc5ceb6975b17554d43d47d6f77ff91a2
nimmalapalli <nimmalapallinarendra@gmail.com>
2024-02-17 14:42:11 +0530

    code

--- src/app/date-range.pipe.ts
+++ src/app/date-range.pipe.ts
@@ -5,12 +5,30 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateRangePipe implements PipeTransform {
 
-  transform(operations: any[], ...dates: Date[]): unknown {
-    const dateFrom = dates[0];
-    const dateTo = dates[1];
-    return operations.filter(operation => {
-      // logic to compare dateFrom and dateTo andthe operation.createdDate
-    });
-  }
+  transform(row: any, f1: Date, f2: Date): any {
+
+    const resultadoFiltro = [];
+    let date1 = new Date(f1);
+    let date2 = new Date(f2);
+    // si es menor a la fecha final
+    if (f1 >= f2 || f1 == null) {
+      return row;
+    }
+    // si el argumento de fecha final es invalido, se setea a la fecha actual
+    if (f2 == null) {
+      f2 = new Date();
+    }
+    // si ambos arreglos son correctos entonces se crea el nuevo arrego
+    for (const filteredRow of row) {
+      let a = new Date(filteredRow.fecha);
+      console.log(a);
+    
+      if (a > date1 && a <= date2) {
+        console.log("asd", filteredRow);
+        resultadoFiltro.push(filteredRow);
+      }
+    }
+    return resultadoFiltro;
+      }
 
 }
