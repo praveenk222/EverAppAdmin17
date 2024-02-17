99a8f26bd8cf4fc89aab594662a315f8f332a86b
praveenk222 <praveenkadma222@gmail.com>
2024-02-17 20:16:36 +0530

    dd

--- src/app/settings/adminuserdetails/adminuserdetails.component.ts
+++ src/app/settings/adminuserdetails/adminuserdetails.component.ts
@@ -20,18 +20,10 @@ export class AdminuserdetailsComponent {
           "link": "/dashboard",
           "UserID": "100",
           "Description":"Test",
-          "CanAccess":1
+          "CanAccess":true,
+          "IsChanged":false
       },
-      {
-          "TabID": 2,
-          "TabName": "Booking History",
-          "icon": "bookmark",
-          "link": "/bookinghistory",
-          "UserID": "100",
-          "CanAccess":1
-
-      },
-    
+     
      
   
   ]
@@ -49,8 +41,56 @@ export class AdminuserdetailsComponent {
   usertabdetails() {
     this.us.getNavById(this.userid).subscribe((res:any)=>{
       this.Details=res;
-      console.log(res)
+      let daata={
+        "TabID": 0,
+        "TabName": "All",
+        "icon": "",
+        "link": "",
+        "UserID": "0",
+        "Description":"User will have all the permissions and can do anything",
+        "CanAccess":false,
+        "IsChanged":false
+      }
+      this.Details.unshift(daata)
+      // this.Details
+      console.log('tablist',res)
     })
   }
-  
+  handleChange(event: any,item:any,i:number) {
+    if (event.checked) {
+    
+          if(item.TabID == 0){
+
+            this.Details.forEach(x=>x.CanAccess =true)
+            this.Details.forEach(x=>x.IsChanged =true)
+          }else{
+            this.Details[i]['IsChanged']=true
+            this.Details[i].CanAccess=true;
+         
+
+          }
+      // Perform actions when checkbox is checked
+    } else {
+          if(item.TabID == 0){
+
+            this.Details.forEach(x=>x.CanAccess =false)
+            this.Details.forEach(x=>x.IsChanged =true)
+
+          }else{
+            this.Details[i].CanAccess=false
+            this.Details[i]['IsChanged']=true
+          }
+
+      // Perform actions when checkbox is unchecked
+    }
+  }
+  save(){
+   let data=this.Details.filter(x=>x.IsChanged == true);
+    this.us.saveUserTabAccess(data).subscribe(res=>{
+      console.log(res)
+      if(res.status){
+        this.usertabdetails()
+      }
+    })
+  }
 }
